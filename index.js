165929ea9d9b7cfc24ec2b7c1d69a9292a474dd9
tripathi-suraj <surajtripathi781@gmail.com>
2022-03-18 22:54:03 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log('console', {} + []);
+
+function a() {
+  return `Hello`;
+}
+var sent = a('hi');
+console.log(sent);
+
+//this keyword?
